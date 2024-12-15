d584bb25a9e25905f04fef1ad562f8796c16096f
djelidrh99 <djelida871@gmail.com>
2024-12-15 13:46:07 +0000

    server js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), // Lier l'hôte à 0.0.0.0
+  port: env.int('PORT', 1337), // Utiliser le port depuis les variables d'environnement ou 1337 par défaut
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  // Configuration du serveur pour augmenter les timeouts
+  server: {
+    keepAliveTimeout: 120000, // 120 secondes
+    headersTimeout: 120000,   // 120 secondes
   },
 });
